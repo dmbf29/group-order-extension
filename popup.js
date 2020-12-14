e0f5ef1f85367505d772ed4d11b89554e80f893c
Douglas Berkley <douglasmberkley@gmail.com>
2020-12-14 16:07:49 +0900

    trying to load js

---
+++ popup.js
@@ -0,0 +1,9 @@
+function buildHTML() {
+  const serviceCharge = document.querySelector('#main-content > div.b5.b6.b7.b8.p8.bc.f1 > div.k2.pc > ul > li:nth-child(3) > div:nth-child(2) > span')
+  const deliveryCharge = document.querySelector('#delivery-charge')
+  console.log(serviceCharge)
+  console.log(deliveryCharge)
+}
+
+buildHTML();
+console.log("hmmmm")
