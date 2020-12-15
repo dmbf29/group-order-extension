a527fa204fc7f268d159c80a3edfe8d907c5ae82
Douglas Berkley <douglasmberkley@gmail.com>
2020-12-15 22:08:46 +0900

    trying to wait for page to load

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -1,7 +1,19 @@
+function scrapeHTML() {
+  // const serviceCharge = document.querySelector('#main-content > div.b5.b6.b7.b8.p8.bc.f1 > div.k2.pc > ul > li:nth-child(3) > div:nth-child(2) > span')
+  // const deliveryCharge = document.querySelector('#delivery-charge')
+  // console.log(serviceCharge)
+  // console.log(deliveryCharge)
+  const list = document.querySelector('[role="search"]').nextElementSibling.children[1].children[0].children[2].querySelector('ul')
+  console.log(list)
+}
+
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.action === 'scrape') scrapeHTML();
-  }
+  document.addEventListener("DOMContentLoaded",
+    function(request, sender, sendResponse) {
+      console.log(request);
+      if (request.action === 'scrape') scrapeHTML();
+    }
+  )
 );
 
 // Our image replacement script
@@ -12,10 +24,3 @@ chrome.runtime.onMessage.addListener(
 //   })
 // }
 
-function scrapeHTML() {
-  document.style.backgroundColor = 'red';
-  const serviceCharge = document.querySelector('#main-content > div.b5.b6.b7.b8.p8.bc.f1 > div.k2.pc > ul > li:nth-child(3) > div:nth-child(2) > span')
-  const deliveryCharge = document.querySelector('#delivery-charge')
-  console.log(serviceCharge)
-  console.log(deliveryCharge)
-}
